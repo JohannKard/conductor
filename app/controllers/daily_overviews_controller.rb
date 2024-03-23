9c36d1ee7b46d7e00427b1064d3c9397fc8bc04e
JohannKard <JohannKard@gmail.com>
2024-03-23 13:14:26 -0500

    little clean up

--- app/controllers/daily_overviews_controller.rb
+++ app/controllers/daily_overviews_controller.rb
@@ -2,7 +2,7 @@ class DailyOverviewsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @date = params[:id] ? Date.parse(params[:id]) : Date.today
     @todos = current_user.todos.where(due_date: @date)
     @habits = current_user.habits.includes(:habit_completions).where(week: current_user.weeks.find_by(start_date: @date.beginning_of_week, end_date: @date.end_of_week))
     @notes = current_user.notes.where(date: @date)
