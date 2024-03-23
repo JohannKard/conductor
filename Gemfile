daac6c9a6482220c8f880fc4a118acde215a4e46
JohannKard <JohannKard@gmail.com>
2024-03-23 13:02:44 -0500

    looking better already :)

--- Gemfile
+++ Gemfile
@@ -46,6 +46,8 @@ gem "bootsnap", require: false
 
 gem "devise"
 
+gem "font-awesome-sass"
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   resources :notes, only: [:create, :destroy]
   resources :weeks, only: [:index, :create, :update, :destroy]
   resources :notes_history, only: [:index, :show]
-  root 'daily_overviews#show', id: Date.today.to_s
+  root 'daily_overviews#show', date: Date.today.to_s
 end
